ddc635f5405cf95f5982b0e5cbe331c455886162
Dacer <qqwerrd@gmail.com>
2018-01-13 01:27:06 +0800

    add LeakCanary
    Former-commit-id: 7cd9ee309eba0a5de45e18ac0780e55105fc91a2

--- app/src/main/java/im/dacer/kata/App.kt
+++ app/src/main/java/im/dacer/kata/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.util.Log
 import com.baoyz.treasure.Treasure
 import com.facebook.stetho.Stetho
+import com.squareup.leakcanary.LeakCanary
 import im.dacer.kata.core.BigBang
 import im.dacer.kata.core.action.CopyAction
 import im.dacer.kata.core.action.ShareAction
@@ -16,6 +17,13 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
+
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         } else {
