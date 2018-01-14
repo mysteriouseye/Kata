65f5ab0112ab8f8899f344b4221582b97933d460
Dacer <qqwerrd@gmail.com>
2018-01-14 18:21:40 +0800

    add duckduckgo

--- app/src/main/java/im/dacer/kata/SearchEngine.java
+++ app/src/main/java/im/dacer/kata/SearchEngine.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import com.baoyz.treasure.Treasure;
 
 import im.dacer.kata.core.action.BingSearchAction;
+import im.dacer.kata.core.action.DuckDuckGoSearchAction;
 import im.dacer.kata.core.action.GoogleSearchAction;
 import im.dacer.kata.core.action.SearchAction;
 
@@ -17,21 +18,24 @@ import im.dacer.kata.core.action.SearchAction;
  */
 public class SearchEngine {
 
-    public static final String BING = "Bing";
     public static final String GOOGLE = "Google";
+    public static final String BING = "Bing";
+    public static final String DUCKDUCKGO = "DuckDuckGo";
 
     public static SearchAction getSearchAction(Context context) {
         Config config = Treasure.get(context, Config.class);
         switch (config.getSearchEngine()) {
-            case BING:
-                return BingSearchAction.create();
             case GOOGLE:
                 return GoogleSearchAction.create();
+            case BING:
+                return BingSearchAction.create();
+            case DUCKDUCKGO:
+                return DuckDuckGoSearchAction.create();
         }
         return null;
     }
 
     public static String[] getSupportSearchEngineList() {
-        return new String[]{BING, GOOGLE};
+        return new String[]{GOOGLE, BING, DUCKDUCKGO};
     }
 }
