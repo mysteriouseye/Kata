b6a419e4722860e565fbf16fecdd2f647d6c8973
Dacer <qqwerrd@gmail.com>
2018-02-11 06:10:10 +0800

    minor changes for chinese

--- core/src/main/java/im/dacer/kata/core/util/LangUtils.kt
+++ core/src/main/java/im/dacer/kata/core/util/LangUtils.kt
@@ -16,18 +16,17 @@ import java.util.*
  */
 class LangUtils(private val appPre: MultiprocessPref) {
 
-    val shouldTranslation get() = appPre.targetLang != DEFAULT_TARGET_LANG_KEY
-
     fun fetchTranslation(dictEntry: DictEntry): Observable<String> {
-        val targetLang = appPre.targetLang
+        var targetLang = appPre.targetLang
+        if (!LANG_KEY_LIST.contains(targetLang)) targetLang = LANG_ENGLISH_KEY
+
+        //since people in China cannot use Google..
+        if (targetLang == LANG_CHINESE_KEY) {
+            return Observable.just(dictEntry.gloss_cn())
+        }
 
         if (targetLang == DEFAULT_TARGET_LANG_KEY) {
             return Observable.just(dictEntry.gloss())
-        }
-
-        //since people in China cannot use Google..
-        if (targetLang == "zh-CN") {
-            return Observable.just(dictEntry.gloss_cn())
         }
 
         return Rx2AndroidNetworking.get(getTranslationUrl(targetLang, dictEntry.gloss()!!))
@@ -46,22 +45,32 @@ class LangUtils(private val appPre: MultiprocessPref) {
     }
 
     companion object {
+        const val LANG_ENGLISH_KEY = "en"
+        const val LANG_CHINESE_KEY = "zh-CN"
+        private const val LANG_ENGLISH_VALUE = "English"
+
         fun getTranslationUrl(target: String, source: String) =
                 "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=$target&dt=t&q=${source.urlEncode()}"
 
-        fun isZhCN(): Boolean = Locale.getDefault().language == "zh" && Locale.getDefault().country == "CN"
+        fun isZhCN(): Boolean {
+            return Locale.getDefault().language == "zh" && Locale.getDefault().country == "CN"
+
+        }
 
         fun getLangByKey(key: String): String {
             val index = LANG_KEY_LIST.toList().indexOf(key)
             return if (index == -1 || index > LANG_LIST.size) {
-                "English"
+                LANG_ENGLISH_VALUE
             } else {
                 LANG_LIST[index]
             }
         }
+        val LANG_LIST = arrayOf(LANG_ENGLISH_VALUE, "Afrikaans", "Albanian", "Arabic", "Azerbaijani", "Basque", "Belarusian", "Bengali", "Bulgarian", "Catalan", "Chinese Simplified", "Chinese Traditional ", "Croatian", "Czech", "Danish", "Dutch", "Esperanto", "Estonian", "Filipino", "Finnish", "French", "Galician", "Georgian", "German", "Greek", "Gujarati", "Haitian Creole", "Hebrew", "Hindi", "Hungarian", "Icelandic", "Indonesian", "Irish", "Italian", "Japanese", "Kannada", "Korean", "Latin", "Latvian", "Lithuanian", "Macedonian", "Malay", "Maltese", "Norwegian", "Persian", "Polish", "Portuguese", "Romanian", "Russian", "Serbian", "Slovak", "Slovenian", "Spanish", "Swahili", "Swedish", "Tamil", "Telugu", "Thai", "Turkish", "Ukrainian", "Urdu", "Vietnamese", "Welsh", "Yiddish")
+        val LANG_KEY_LIST = arrayOf(LANG_ENGLISH_KEY, "af", "sq", "ar", "az", "eu", "be", "bn", "bg", "ca", "zh-CN", "zh-TW", "hr", "cs", "da", "nl", "eo", "et", "tl", "fi", "fr", "gl", "ka", "de", "el", "gu", "ht", "iw", "hi", "hu", "is", "id", "ga", "it", "ja", "kn", "ko", "la", "lv", "lt", "mk", "ms", "mt", "no", "fa", "pl", "pt", "ro", "ru", "sr", "sk", "sl", "es", "sw", "sv", "ta", "te", "th", "tr", "uk", "ur", "vi", "cy", "yi")
 
-        const val DEFAULT_TARGET_LANG_KEY = "en"
-        val LANG_LIST = arrayOf("English", "Afrikaans", "Albanian", "Arabic", "Azerbaijani", "Basque", "Belarusian", "Bengali", "Bulgarian", "Catalan", "Chinese Simplified", "Chinese Traditional ", "Croatian", "Czech", "Danish", "Dutch", "Esperanto", "Estonian", "Filipino", "Finnish", "French", "Galician", "Georgian", "German", "Greek", "Gujarati", "Haitian Creole", "Hebrew", "Hindi", "Hungarian", "Icelandic", "Indonesian", "Irish", "Italian", "Japanese", "Kannada", "Korean", "Latin", "Latvian", "Lithuanian", "Macedonian", "Malay", "Maltese", "Norwegian", "Persian", "Polish", "Portuguese", "Romanian", "Russian", "Serbian", "Slovak", "Slovenian", "Spanish", "Swahili", "Swedish", "Tamil", "Telugu", "Thai", "Turkish", "Ukrainian", "Urdu", "Vietnamese", "Welsh", "Yiddish")
-        val LANG_KEY_LIST = arrayOf("en", "af", "sq", "ar", "az", "eu", "be", "bn", "bg", "ca", "zh-CN", "zh-TW", "hr", "cs", "da", "nl", "eo", "et", "tl", "fi", "fr", "gl", "ka", "de", "el", "gu", "ht", "iw", "hi", "hu", "is", "id", "ga", "it", "ja", "kn", "ko", "la", "lv", "lt", "mk", "ms", "mt", "no", "fa", "pl", "pt", "ro", "ru", "sr", "sk", "sl", "es", "sw", "sv", "ta", "te", "th", "tr", "uk", "ur", "vi", "cy", "yi")
+
+
+        var DEFAULT_TARGET_LANG_KEY: String = LANG_ENGLISH_KEY
+            get() = if (isZhCN()) LANG_CHINESE_KEY else LANG_ENGLISH_KEY
     }
 }
